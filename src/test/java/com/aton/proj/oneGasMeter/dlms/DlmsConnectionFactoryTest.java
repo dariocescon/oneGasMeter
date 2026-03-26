26ef805057c0298cffb27d2f4591b53c9eff9ccb
Dario.Cescon <Dario.Cescon@aton.com>
2026-03-26 12:40:41 +0100

    Excluded TCP server for test

--- src/test/java/com/aton/proj/oneGasMeter/dlms/DlmsConnectionFactoryTest.java
+++ src/test/java/com/aton/proj/oneGasMeter/dlms/DlmsConnectionFactoryTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.aton.proj.oneGasMeter.config.DlmsClientConfig;
 import com.aton.proj.oneGasMeter.dlms.transport.DlmsTransport;
 
-@SpringBootTest
+@SpringBootTest(properties = "dlms.inbound.enabled=false")
 class DlmsConnectionFactoryTest {
 
     @Autowired
